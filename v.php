f0a454f10da57b5bda238f02256d8e355be492b6
mrhooda <99620696+mrhooda@users.noreply.github.com>
2024-11-17 21:56:42 +0200

    Add files via upload

---
+++ v.php
@@ -0,0 +1,119 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<title>video player</title>
+<meta name="description" property="og:description" content="video player ." />
+<meta name="author" content="Sam Potts" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+<link rel="icon" href="https://cdn.plyr.io/static/icons/favicon.ico" />
+<link rel="icon" type="image/png" href="https://cdn.plyr.io/static/icons/32x32.png" sizes="32x32" />
+<link rel="icon" type="image/png" href="https://cdn.plyr.io/static/icons/16x16.png" sizes="16x16" />
+<link rel="apple-touch-icon" sizes="180x180" href="https://cdn.plyr.io/static/icons/180x180.png" />
+
+<meta property="og:title" content="video player" />
+<meta property="og:site_name" content="Plyr" />
+<meta property="og:url" content="https://plyr.io" />
+<meta property="og:image" content="https://cdn.plyr.io/static/icons/1200x630.png" />
+
+<meta name="twitter:card" content="summary" />
+<meta name="twitter:site" content="@sam_potts" />
+<meta name="twitter:creator" content="@sam_potts" />
+<meta name="twitter:card" content="summary_large_image" />
+
+<link rel="stylesheet" href="https://cdn.plyr.io/3.7.3/demo.css" />
+
+<link rel="preload" as="font" crossorigin type="font/woff2" href="https://cdn.plyr.io/static/fonts/gordita-medium.woff2" />
+<link rel="preload" as="font" crossorigin type="font/woff2" href="https://cdn.plyr.io/static/fonts/gordita-bold.woff2" />
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-132699580-1"></script>
+<script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag('js', new Date());
+      gtag('config', 'UA-132699580-1');
+    </script>
+    <script type='text/javascript' src='//italianbeepimpediment.com/1b/6f/a1/1b6fa1d1c7ac9d37e2e43640be75e2a8.js'></script>
+</head>
+<body>
+<script type="text/javascript">
+	atOptions = {
+		'key' : '93f87769eea32db13683c39dbfb20a9b',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 400,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//italianbeepimpediment.com/93f87769eea32db13683c39dbfb20a9b/invoke.js"></scr' + 'ipt>');
+</script>
+<div class="grid">
+<header>
+<h1>Play Video<span>player</span></h1>
+</header>
+<main>
+<div id="container">
+<video controls crossorigin playsinline data-poster="https://b.top4top.io/p_2590d0m2q1.jpg" id="player">
+
+<source src="<?php echo $_GET['v'];?>" type="video/mp4" size="576" />
+<source src="<?php echo $_GET['v'];?>" type="video/mp4" size="720" />
+<source src="<?php echo $_GET['v'];?>" type="video/mp4" size="1080" />
+
+<track kind="captions" label="English" srclang="en" src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.en.vtt" default />
+<track kind="captions" label="Français" srclang="fr" src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.fr.vtt" />
+
+<a href="<?php echo $_GET['v'];?>" download>Download</a>
+</video>
+</div>
+<div class="call-to-action">
+<a href="<?php echo $_GET['v'];?>" target="_blank" class="button js-shr">
+<svg class="icon" role="presentation">
+<title>DOWNLOUD</title>
+<path d="M15.608,6.262h-2.338v0.935h2.338c0.516,0,0.934,0.418,0.934,0.935v8.879c0,0.517-0.418,0.935-0.934,0.935H4.392c-0.516,0-0.935-0.418-0.935-0.935V8.131c0-0.516,0.419-0.935,0.935-0.935h2.336V6.262H4.392c-1.032,0-1.869,0.837-1.869,1.869v8.879c0,1.031,0.837,1.869,1.869,1.869h11.216c1.031,0,1.869-0.838,1.869-1.869V8.131C17.478,7.099,16.64,6.262,15.608,6.262z M9.513,11.973c0.017,0.082,0.047,0.162,0.109,0.226c0.104,0.106,0.243,0.143,0.378,0.126c0.135,0.017,0.274-0.02,0.377-0.126c0.064-0.065,0.097-0.147,0.115-0.231l1.708-1.751c0.178-0.183,0.178-0.479,0-0.662c-0.178-0.182-0.467-0.182-0.645,0l-1.101,1.129V1.588c0-0.258-0.204-0.467-0.456-0.467c-0.252,0-0.456,0.209-0.456,0.467v9.094L8.443,9.553c-0.178-0.182-0.467-0.182-0.645,0c-0.178,0.184-0.178,0.479,0,0.662L9.513,11.973z"></path>
+</svg>
+Download
+</a>
+</div>
+<br>
+<div class="js-shr">
+<h3> Hello, you can now watch movies and series for free and without ads for free through Telegram
+</h3>
+</div>
+<figure>
+      <a href="<?php echo $url;?>">
+        <img src="<?php echo $img;?>" alt="<?php echo $title;?>">
+      </a>
+      <figcaption>
+        <?php echo $description;?>
+      </figcaption>
+    </figure>
+<aside>
+<svg class="icon">
+<title>Telegram</title>
+<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.287 5.906c-.778.324-2.334.994-4.666 2.01-.378.15-.577.298-.595.442-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294.26.006.549-.1.868-.32 2.179-1.471 3.304-2.214 3.374-2.23.05-.012.12-.026.166.016.047.041.042.12.037.141-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8.154 8.154 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629.093.06.183.125.27.187.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.426 1.426 0 0 0-.013-.315.337.337 0 0 0-.114-.217.526.526 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09z"></path>
+</svg>
+<p>
+Me Telegram,
+<a href="https://t.me/BlIJJ" target="_blank" class="js-shr">@BlIJJ</a>
+</p>
+</aside>
+<br>
+<aside>
+<svg class="icon">
+<title>Telegram</title>
+<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.287 5.906c-.778.324-2.334.994-4.666 2.01-.378.15-.577.298-.595.442-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294.26.006.549-.1.868-.32 2.179-1.471 3.304-2.214 3.374-2.23.05-.012.12-.026.166.016.047.041.042.12.037.141-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8.154 8.154 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629.093.06.183.125.27.187.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.426 1.426 0 0 0-.013-.315.337.337 0 0 0-.114-.217.526.526 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09z"></path>
+</svg>
+<p>
+BY &copy 2023 Bot,
+<a href="https://t.me/Fushar_bot" target="_blank" class="js-shr">@Fushar_bot</a>
+</p>
+</aside>
+<script src="https://cdn.plyr.io/3.7.3/demo.js" crossorigin="anonymous"></script>
+<script async="async" data-cfasync="false" src="//italianbeepimpediment.com/6c67571c352aae4b81577f903fe69ebd/invoke.js"></script>
+<div id="container-6c67571c352aae4b81577f903fe69ebd"></div>
+<script type='text/javascript' src='//italianbeepimpediment.com/20/77/41/20774169897761693bfd30c2ea96eb8e.js'></script>
+</body>
+</html>
